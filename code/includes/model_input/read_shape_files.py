2c97f924a1e3d3c007937eb101502caa90420375
diegomartinez33 <da.martinez33@uniandes.edu.co>
2021-07-21 14:49:58 -0500

    new files

---
+++ code/includes/model_input/read_shape_files.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import numpy as np
+import geopandas as gpd
+from geopy import distance
+from shapely.geometry import Polygon
+
+shp1 = gpd.read_file(os.path.join('.', 'MVI2020','MVI2020.shp'))
+shp2 = gpd.read_file(os.path.join('.', 'ZONAS','ZAT.shp'))
+
+for col in shp1.columns:
+	print(col)
+
+for col in shp2.columns:
+	print(col)
